4885078d5da3ef6aa48790264b5c89fb8b0e1cd6
sterinaldi <--get>
2021-01-11 19:16:54 +0100

    complete draft

--- gibbs_sampler.py
+++ gibbs_sampler.py
@@ -1,11 +1,22 @@
 import numpy as np
 from numpy.random import uniform
 import random as rd
-
+import matplotlib.pyplot as plt
+import os
 
 class gibbs_sampler:
     
-    def __init__(self, samples, mass_boundaries, sigma_boundaries, n_draws, burnin, step, alpha0, gamma):
+    def __init__(self,
+                 samples,
+                 mass_boundaries,
+                 sigma_boundaries,
+                 n_draws,
+                 burnin,
+                 step,
+                 alpha0,
+                 gamma,
+                 output_folder = os.getcwd(),
+                 n_resamples = 250):
         
         self.samples     = samples
         self.table_index  = []
@@ -35,11 +46,12 @@ class gibbs_sampler:
         self.draw_sigma  = lambda : np.exp(uniform(self.min_sigma,self.max_sigma))
         
         # Configuration parameters
-        self.alpha0  = alpha0
-        self.gamma   = gamma
-        self.n_draws = n_draws # total number of outcomes
-        self.burnin  = burnin  # burn-in
-        self.step    = step    # steps between two outcomes (avoids autocorrelation)
+        self.alpha0      = alpha0
+        self.gamma       = gamma
+        self.n_draws     = n_draws     # total number of outcomes
+        self.burnin      = burnin      # burn-in
+        self.step        = step        # steps between two outcomes (avoids autocorrelation)
+        self.n_resamples = n_resamples # bootstrap resamplings
         
         self.mass_samples = []
         self.initialise_tables()
@@ -147,9 +159,93 @@ class gibbs_sampler:
             
     def evaluate_probability_sample(self, sample):
         return (np.sum([normal_density(sample, *self.components(index)) for table in self.tables for index in table]) + self.gamma * self.samples_prior(sample))/(np.sum([len(t) for t in self.tables])+self.gamma)
-
-
+        
     def normal_density(x, x0, sigma):
         return np.exp(-(x-x0)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
     
     
+    def markov_step(self):
+        for event_index in range(len(samples)):
+            for sample_index in range(len(samples[event_index])):
+                self.update_table(sample_index, event_index)
+            for component_index in range(len(tables[event_index])):
+                self.update_component(component_index, event_index)
+        return
+    
+    def save_mass_samples(self):
+        self.mass_samples.append([self.components[index][0] for table in self.tables for index in table])
+    
+    def run_sampling(self):
+        for i in range(self.burnin):
+            self.markov_step()
+            print('\rBURN-IN: {0}/{1}'.format(i+1, self.burnin))
+        print('\n')
+        for i in range(self.n_draws):
+            print('\rSAMPLING: {0}/{1}'.format(i+1, self.n_draws))
+            for j in range(self.step):
+                self.markov_step()
+            self.save_mass_samples()
+            print('\n')
+        self.mass_samples = np.array([m for draw in self.mass_samples for m in draw])
+        return
+    
+    def single_bootstrap(self):
+        samples = choice(self.mass_samples, len(self.mass_samples))
+        heights, bins, patches = plt.hist(samples, bins = self.bins, density = True)
+        self.resampled_bins.append(heights)
+        return
+    
+    def bootstrap(self):
+        self.resampled_bins = []
+        for i in range(self.n_resamples):
+            print('\rBOOTSTRAP: {0}/{1}'.format(i, self.n_resamples))
+            self.single_bootstrap()
+        print('\n')
+        self.means  = np.array(self.resampled_bins).mean(axis = 1)
+        self.errors = np.array(self.resampled_bins).std(axis = 1)
+        return
+    
+    def run(self):
+        self.run_sampling()
+        np.savetxt(self.output+'/mass_samples.txt', self.mass_samples)
+        
+        # samples
+        fig = plt.figure(1)
+        ax  = fig.add_subplot(111)
+        self.heights, self.bins, self.patches = ax.hist(self.mass_samples, bins = 'auto', density = True)
+        ax.set_xlabel('$M_1\ [M_\\odot]$')
+        plt.savefig(self.output+'/mass_samples.pdf', bbox_inches = 'tight')
+        
+        self.ref_bins = (self.bins + (self.bins[1]-self.bins[0])/2)[:-1]
+        self.bootstrap()
+        # bootstrapped samples
+        fig = plt.figure(2)
+        ax  = ax.fig.add_subplot(111)
+        ax.fill_between(self.ref_bins, self.means+self.errors, self.means-self.errors, alpha=0.5, edgecolor='#3F7F4C', facecolor='aquamarine')
+        ax.plot(self.ref_bins, self.heights, ls = '--', c = 'green')
+        ax.set_xlabel('$M_1\ [M_\\odot]$')
+        ax.set_ylabel('$p(M)$')
+        plt.savefig(self.output+'/distribution.pdf', bbox_inches = 'tight')
+    
+    def postprocessing(self, samples_file = None, bootstrapping = False):
+        if samples_file is not None:
+            self.mass_samples = np.genfromtxt(samples_file)
+        if bootstrapping:
+            self.bootstrap()
+        # samples
+        fig = plt.figure(1)
+        ax  = fig.add_subplot(111)
+        self.heights, self.bins, self.patches = ax.hist(self.mass_samples, bins = 'auto', density = True)
+        ax.set_xlabel('$M_1\ [M_\\odot]$')
+        plt.savefig(self.output+'/mass_samples.pdf', bbox_inches = 'tight')
+        
+        self.ref_bins = (self.bins + (self.bins[1]-self.bins[0])/2)[:-1]
+        self.bootstrap()
+        # bootstrapped samples
+        fig = plt.figure(2)
+        ax  = ax.fig.add_subplot(111)
+        ax.fill_between(self.ref_bins, self.means+self.errors, self.means-self.errors, alpha=0.5, edgecolor='#3F7F4C', facecolor='aquamarine')
+        ax.plot(self.ref_bins, self.heights, ls = '--', c = 'green')
+        ax.set_xlabel('$M_1\ [M_\\odot]$')
+        ax.set_ylabel('$p(M)$')
+        plt.savefig(self.output+'/distribution.pdf', bbox_inches = 'tight')
