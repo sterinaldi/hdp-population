7b244d0a00014e29d986901d091a0f4ff359242a
sterinaldi <--get>
2021-01-12 17:00:21 +0100

    updated to work with Gibbs sampler

--- generate_events.py
+++ generate_events.py
@@ -1,12 +1,36 @@
 import numpy as np
-from numpy.random import normal
+import os
+from numpy.random import normal, uniform, choice
+import sampler
+import matplotlib.pyplot as plt
 
-out_dir   = '/Users/stefanorinaldi/Documents/mass_inference/events/'
-n_events  = 100
-n_samples = 1000
+def normal_density(x, x0, sigma):
+    return np.exp(-(x-x0)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
 
+def draw_sample(mu1, mu2, s1, s2):
+    while(1):
+        m = uniform(5, 50)
+        if (normal_density(m, mu1, s1) + normal_density(m, mu2, s2)) > uniform(0, max([normal_density(mu1,mu1,s1), normal_density(mu2,mu2,s2)])):
+            return m
+
+out_dir   = '/Users/stefanorinaldi/Documents/mass_inference/multivariate-25-35/events/'
+n_events  = 15
+n_samples = 100
+alpha     = 100
+pars      = [25,4]
+samples = []
+
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+mu = []
 for i in range(n_events):
-    M1 = normal(30,7)
-    file = out_dir + 'event_%d.txt' %(i+1)
-    np.savetxt(file, normal(M1, 3, n_samples))
-
+    pars = [draw_sample(25, 35, 3, 2), np.exp(uniform(np.log(2), np.log(6)))]
+    for _ in range(n_samples):
+        if uniform() < alpha/(alpha+len(samples)):
+            samples.append(normal(*pars))
+        else:
+            samples.append(choice(samples))
+    mu.append(pars[0])
+    np.savetxt(out_dir+'event_{0}.txt'.format(i+1), np.array(samples))
+    samples = []
