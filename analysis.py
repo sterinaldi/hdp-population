9ec48201f2343f82685eb0b20b61cc1f0e4a751f
sterinaldi <--get>
2021-01-13 23:43:54 +0100

    implemented multiprocessing

--- analysis.py
+++ analysis.py
@@ -2,48 +2,49 @@ import numpy as np
 import os
 import gibbs_sampler as GS
 import ray
-from ray.util import ActorPool
 
-events_path = '/Users/stefanorinaldi/Documents/mass_inference/gaussian-40/events/'
+events_path = '/Users/stefanorinaldi/Documents/mass_inference/multivariate-event/events/'
 event_files = [f for f in os.listdir(events_path) if not f.startswith('.')]
 events      = []
-output      = '/Users/stefanorinaldi/Documents/mass_inference/gaussian-40'
+output      = '/Users/stefanorinaldi/Documents/mass_inference/multivariate-event'
 
 for event in event_files:
     events.append(np.genfromtxt(events_path+event))
 
 def normal_density(x, x0, sigma):
     return np.exp(-(x-x0)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
-    
 
+@ray.remote
+def wrapper(sampler):
+    sampler.run()
+    return sampler.get_mass_samples()
 
 ray.init()
 pars = [25,4]
 pars_1 = [15, 2]
 pars_2 = [35, 3]
 
-samp = []
-n_parallels = 4
+samplers = []
+n_parallel_jobs = 4
 
-for i in range(n_parallels):
-    samp.append(GS.gibbs_sampler.remote(samples = events,
+for i in range(n_parallel_jobs):
+    samplers.append(GS.gibbs_sampler(samples = events,
                         mass_b  = [5,50],
-                        n_draws = 1000,
-                        burnin  = 1000,
+                        n_draws = 10,
+                        burnin  = 100,
                         step    = 10,
                         alpha0  = 10,
                         gamma   = 10,
                         output_folder = output,
-                        verbose = False,
+                        verbose = True,
                         # injected_density = lambda x : normal_density(x, *pars)
                         injected_density = lambda x : (normal_density(x, *pars_1) + normal_density(x, *pars_2))/2.
                         ))
                         
-
-pool = ActorPool(samp)
-S = pool.map_unordered(lambda a, _ : a.run.remote(), range(4))
-for s in S:
-    print('Done')
-np.savetxt(output+'/mass_samples.txt', np.array([m for s in samp for m in s.mass_samples]))
+tasks = []
+for sampler in samplers:
+    tasks.append(wrapper.remote(sampler))
+mass_samples = [m for list in ray.get(tasks) for m in list]
+np.savetxt(output+'/mass_samples.txt', np.array([mass_samples]))
 # producing combined posterior plots
-samp[0].postprocessing(output+'/mass_samples.txt', bootstrapping = True)
+samplers[0].postprocessing(output+'/mass_samples.txt', bootstrapping = True)
