7c0431352724dc07fa80c6f6af8542cc8965f2ea
sterinaldi <--get>
2021-02-18 11:10:07 +0100

    updated to work with recent class modifications

--- test_dpgmm.py
+++ test_dpgmm.py
@@ -2,10 +2,10 @@ import numpy as np
 import os
 import collapsed_gibbs as DPGMM
 
-events_path = '/Users/stefanorinaldi/Documents/mass_inference/uniform/events/'
+events_path = '/Users/stefanorinaldi/Documents/mass_inference/universe/events/'
 event_files = [f for f in os.listdir(events_path) if not f.startswith('.')]
 events      = []
-output      = '/Users/stefanorinaldi/Documents/mass_inference/uniform/'
+output      = '/Users/stefanorinaldi/Documents/mass_inference/universe/'
 
 for event in event_files:
     events.append(np.genfromtxt(events_path+event))
@@ -19,15 +19,15 @@ pars_1 = [25, 3]
 pars_2 = [35, 2]
 
 sampler = DPGMM.CGSampler(events = events,
-                        mass_b  = [5,50],
-                        n_draws = 10,
-                        burnin  = 100,
-                        step    = 10,
-                        alpha0  = 10,
+                        #mass_b  = [5,50],
+                        n_draws = 100,
+                        burnin  = 1000,
+                        step    = 100,
+                        alpha0  = 2,
                         gamma0   = 1,
                         output_folder = output,
                         # injected_density = lambda x : normal_density(x, *pars)
                         # injected_density = lambda x : (normal_density(x, *pars_1) + normal_density(x, *pars_2))/2.
                         )
                         
-sampler.run()
+sampler.run_event_sampling()
