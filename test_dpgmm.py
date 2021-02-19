12a7a4b1d96ea9d96779b14649664add7d93cc6c
sterinaldi <--get>
2021-02-19 16:50:12 +0100

    update

--- test_dpgmm.py
+++ test_dpgmm.py
@@ -2,10 +2,10 @@ import numpy as np
 import os
 import collapsed_gibbs as DPGMM
 
-events_path = '/Users/stefanorinaldi/Documents/mass_inference/multivariate-25-35/events/'
+events_path = '/Users/stefanorinaldi/Documents/mass_inference/universe_1/events/'
 event_files = [f for f in os.listdir(events_path) if not f.startswith('.')]
 events      = []
-output      = '/Users/stefanorinaldi/Documents/mass_inference/multivariate-25-35/'
+output      = '/Users/stefanorinaldi/Documents/mass_inference/universe_1/'
 
 for event in event_files:
     events.append(np.genfromtxt(events_path+event))
@@ -14,18 +14,19 @@ def normal_density(x, x0, sigma):
     return np.exp(-(x-x0)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
 
 
-pars = [25,4]
-pars_1 = [25, 3]
+pars = [30,3]
+pars_1 = [30, 3]
 pars_2 = [35, 2]
 
 sampler = DPGMM.CGSampler(events = events,
                         #mass_b  = [5,50],
-                        samp_settings = [100,10,10],
+                        samp_settings = [100,10,100],
                         alpha0  = 1,
-                        gamma0   = 1,
+                        gamma0   = 3,
+                        delta_M = 2,
                         output_folder = output,
-                        process_events = True
-                        # injected_density = lambda x : normal_density(x, *pars)
+                        process_events = False,
+                        injected_density = lambda x : normal_density(x, *pars)
                         # injected_density = lambda x : (normal_density(x, *pars_1) + normal_density(x, *pars_2))/2.
                         )
                         
