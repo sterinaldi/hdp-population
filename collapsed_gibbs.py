c1aeca77eb7e1aea161a255925bffbfc57f44a12
sterinaldi <--get>
2021-02-26 16:17:56 +0100

    implemented NIX sampling

--- collapsed_gibbs.py
+++ collapsed_gibbs.py
@@ -92,7 +92,7 @@ class CGSampler:
         self.output_folder = output_folder
         self.icn = initial_cluster_number
         self.event_samplers = []
-        self.delta_M = np.sqrt(np.std(events, axis = 1))
+        self.delta_M = np.std(events, axis = 1)
         self.verbose = verbose
         self.process_events = process_events
         self.diagnostic = diagnostic
