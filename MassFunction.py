1b575d31d70aebf692e9eb7d1e81e8cdd1cb47e8
sterinaldi <--get>
2021-02-27 18:59:33 +0100

    modified default parameters

--- MassFunction.py
+++ MassFunction.py
@@ -30,8 +30,8 @@ def main():
     parser.add_option("--samp_settings", type = "string", dest = "samp_settings", help = "Burnin, samples and step for MF sampling", default = '1000,100,10')
     parser.add_option("--samp_settings_ev", type = "string", dest = "samp_settings_ev", help = "Burnin, samples and step for single event sampling. If None, uses MF settings")
     parser.add_option("--mc_settings", type = "string", dest = "mc_settings", help = "Burnin and step for mass sampling", default = '100,10')
-    parser.add_option("--hyperpars", type = "string", dest = "hyperpars", help = "MF hyperparameters (a0, b0, V0). See https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf sec. 6 for reference", default = '3,10,4')
-    parser.add_option("--hyperpars_ev", type = "string", dest = "hyperpars_ev", help = "Event hyperparameters (a0, b0, V0)", default = '3,3,4')
+    parser.add_option("--hyperpars", type = "string", dest = "hyperpars", help = "MF hyperparameters (a0, b0, V0). See https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf sec. 6 for reference", default = '1,1,1000')
+    parser.add_option("--hyperpars_ev", type = "string", dest = "hyperpars_ev", help = "Event hyperparameters (a0, b0, V0)", default = '1,1,1000')
     parser.add_option("--alpha", type = "float", dest = "alpha0", help = "Internal (event) concentration parameter", default = 1.)
     parser.add_option("--gamma", type = "float", dest = "gamma0", help = "External (MF) concentration parameter", default = 1.)
     parser.add_option("-e", "--processed_events", dest = "process_events", action = 'store_false', default = True, help = "Disables event processing")
@@ -49,8 +49,10 @@ def main():
         for key, val in zip(vars(options).keys(), vars(options).values()):
             if not is_opt_provided(parser, key):
                 vars(options)[key] = opts[key]
-        if options.true_masses is 'None':
+        if options.true_masses == 'None':
             options.true_masses = None
+        if options.inj_density_file == 'None':
+            options.inj_density_file = None
     options.hyperpars = [float(x) for x in options.hyperpars.split(',')]
     if options.hyperpars_ev is not None:
         options.hyperpars_ev = [float(x) for x in options.hyperpars_ev.split(',')]
