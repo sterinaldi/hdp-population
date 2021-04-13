0d1840b73f69954112eb7331684d78f44de7ea40
sterinaldi <--get>
2021-04-13 14:53:20 +0200

    Initial commit

---
+++ sampler_component_pars.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import logsumexp
+from numpy.random import uniform
+
+def log_norm(x, x0, sigma1, sigma2):
+    return -((x-x0)**2 + sigma2**2)/(2*(sigma1**2 + sigma2**2)) - np.log(np.sqrt(2*np.pi)) - 0.5*np.log(sigma1**2 + sigma2**2)
+
+def posterior(mu, sigma, log_prior, events, sigma_max, m_min, m_max):
+    if not (0 < sigma < sigma_max and m_min < mu < m_max):
+        return -np.inf
+    events_sum = np.sum([logsumexp([np.log(component['w']) + log_norm(mu, component['mu'], sigma, component['sigma']) for component in ev]) for ev in events])
+    return events_sum
+
+def propose_point(old_point, dm, ds):
+    m = old_point[0] + uniform(-1,1)*dm
+    s = old_point[1] + uniform(-1,1)*ds
+    return [m,s]
+
+def sample_point(events, m_min, m_max, s_min, s_max, burnin, dm = 3, ds = 1)
+    old_point = [uniform(m_min, m_max), uniform(s_min, s_max)]
+    for _ in range(burnin):
+        new_point = propose_point(old_point, dm, ds)
+        log_new = log_posterior(new_point[0], new_point[1], log_prior, events)
+        log_old = log_posterior(old_point[0], old_point[1], log_prior, events)
+        if log_new - log_old > np.log(uniform(0,1)):
+            old_point = new_point
+    return old_point
