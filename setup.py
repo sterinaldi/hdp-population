ae160a07048cd6c562f42db78426a97db50776e9
sterinaldi <--get>
2021-04-15 16:35:59 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import numpy
+from setuptools import setup, find_packages
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+from codecs import open
+from os import path
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import os
+
+# see https://stackoverflow.com/a/21621689/1862861 for why this is here
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        self.include_dirs.append(numpy.get_include())
+
+ext_modules=[
+             Extension("predictive",
+                       sources=["predictive.pyx"],
+                       libraries=["m"], # Unix-like specific
+                       extra_compile_args=["-O3","-ffast-math"],
+                       include_dirs=[numpy.get_include()]
+                       )
+             ]
+
+setup(
+      name = "predictive",
+      ext_modules = cythonize(ext_modules, language_level = "3"),
+      include_dirs=[numpy.get_include()]
+      )
