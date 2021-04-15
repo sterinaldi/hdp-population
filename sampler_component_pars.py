73912ee470f0da076ff7e49f94f71ce752305388
sterinaldi <--get>
2021-04-15 16:23:19 +0200

    fixed bug

--- sampler_component_pars.py
+++ sampler_component_pars.py
@@ -6,10 +6,10 @@ from numpy.random import uniform
 def log_norm(x, x0, sigma1, sigma2):
     return -((x-x0)**2)/(2*(sigma1**2 + sigma2**2)) - np.log(np.sqrt(2*np.pi)) - 0.5*np.log(sigma1**2 + sigma2**2)
 
-def log_posterior(mu, sigma, events, sigma_max, m_min, m_max):
-    if not (0 < sigma < sigma_max and m_min < mu < m_max):
+def log_posterior(mu, sigma, events, sigma_min, sigma_max, m_min, m_max):
+    if not (sigma_min < sigma < sigma_max and m_min < mu < m_max):
         return -np.inf
-    events_sum = np.sum([logsumexp([np.log(component['w']) + log_norm(mu, component['mu'], sigma, component['sigma']) for component in ev]) for ev in events])
+    events_sum = np.sum([logsumexp([np.log(component['weight']) + log_norm(mu, component['mean'], sigma, component['sigma']) for component in ev.values()]) for ev in events])
     return events_sum
 
 def propose_point(old_point, dm, ds):
@@ -21,8 +21,8 @@ def sample_point(events, m_min, m_max, s_min, s_max, burnin = 1000, dm = 3, ds =
     old_point = [uniform(m_min, m_max), uniform(s_min, s_max)]
     for _ in range(burnin):
         new_point = propose_point(old_point, dm, ds)
-        log_new = log_posterior(new_point[0], new_point[1], events)
-        log_old = log_posterior(old_point[0], old_point[1], events)
+        log_new = log_posterior(new_point[0], new_point[1], events, s_min, s_max, m_min, m_max)
+        log_old = log_posterior(old_point[0], old_point[1], events, s_min, s_max, m_min, m_max)
         if log_new - log_old > np.log(uniform(0,1)):
             old_point = new_point
     return old_point[0], old_point[1]
