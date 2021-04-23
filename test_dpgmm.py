7cf91c20066d19c1429f277cba504ccf484cb3b9
sterinaldi <--get>
2021-04-23 09:34:07 +0200

    modified folders

--- test_dpgmm.py
+++ test_dpgmm.py
@@ -4,10 +4,10 @@ import collapsed_gibbs as DPGMM
 import ray
 import cpnest.model
 
-events_path = '/Users/stefanorinaldi/Documents/mass_inference/multidim/events/'
+events_path = '/Users/stefanorinaldi/Documents/mass_inference/stars/events/'
 event_files = [f for f in os.listdir(events_path) if not f.startswith('.')]
 events      = []
-output      = '/Users/stefanorinaldi/Documents/mass_inference/multidim/'
+output      = '/Users/stefanorinaldi/Documents/mass_inference/stars/'
 
 for event in event_files:
     events.append(np.genfromtxt(events_path+event))
@@ -16,9 +16,9 @@ def normal_density(x, x0, sigma):
     return np.exp(-(x-x0)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
 
 sampler = DPGMM.CGSampler(events = events,
-                        n_draws = 10,
-                        burnin  = 100,
-                        step    = 10,
+                        n_draws = 100,
+                        burnin  = 10,
+                        step    = 1,
                         alpha0  = 1,
                         output_folder = output,
                         # injected_density = lambda x : normal_density(x, *pars)
