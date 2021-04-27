5dc74692d28e1582845226e3153df59ea6118c27
sterinaldi <--get>
2021-04-27 09:30:21 +0200

    modified parameters

--- test_dpgmm.py
+++ test_dpgmm.py
@@ -1,13 +1,11 @@
 import numpy as np
 import os
 import collapsed_gibbs as DPGMM
-import ray
-import cpnest.model
 
-events_path = '/Users/stefanorinaldi/Documents/mass_inference/stars/events/'
+events_path = '/Users/stefanorinaldi/Documents/mass_inference/stars_2/events/'
 event_files = [f for f in os.listdir(events_path) if not f.startswith('.')]
 events      = []
-output      = '/Users/stefanorinaldi/Documents/mass_inference/stars/'
+output      = '/Users/stefanorinaldi/Documents/mass_inference/stars_2/'
 
 for event in event_files:
     events.append(np.genfromtxt(events_path+event))
@@ -16,7 +14,7 @@ def normal_density(x, x0, sigma):
     return np.exp(-(x-x0)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
 
 sampler = DPGMM.StarClusters(catalog = events[0],
-                        n_draws = 20,
+                        n_draws = 100,
                         burnin  = 10,
                         step    = 1,
                         alpha0  = 1,
