fc5ab43d8149fabb1412fc04bdff57ee9d2700d4
sterinaldi <--get>
2021-05-05 09:53:14 +0200

    fixed bug in saving MF samples

--- collapsed_gibbs.py
+++ collapsed_gibbs.py
@@ -828,7 +828,7 @@ class MF_Sampler():
         print('------------------------')
         return
 
-    def plot_samples(self):
+    def postprocess(self):
         """
         Plots samples [x] for each event in separate plots along with inferred distribution.
         """
@@ -877,12 +877,21 @@ class MF_Sampler():
         extension ='.pkl'
         x = 0
         fileName = name + str(x) + extension
-        if not os.path.exists(fileName):
+        while os.path.exists(fileName):
             x = x + 1
-        fileName = name + str(x) + extension
+            fileName = name + str(x) + extension
         picklefile = open(fileName, 'wb')
         pickle.dump(self.mixture_samples, picklefile)
         picklefile.close()
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(np.arange(1,len(self.n_clusters)+1), self.n_clusters, ls = '--', marker = ',', linewidth = 0.5)
+        fig.savefig(self.output_events+'n_clusters_mf.pdf', bbox_inches='tight')
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.hist(self.alpha_samples, bins = int(np.sqrt(len(self.alpha_samples))))
+        fig.savefig(self.output_events+'/gamma_mf.pdf', bbox_inches='tight')
 
         if self.diagnostic:
             fig = plt.figure()
@@ -948,15 +957,7 @@ class MF_Sampler():
         self.output_events = self.output_folder
         if not os.path.exists(self.output_events):
             os.mkdir(self.output_events)
-        self.plot_samples()
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(np.arange(1,len(self.n_clusters)+1), self.n_clusters, ls = '--', marker = ',', linewidth = 0.5)
-        fig.savefig(self.output_events+'n_clusters_mf.pdf', bbox_inches='tight')
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.hist(self.alpha_samples, bins = int(np.sqrt(len(self.alpha_samples))))
-        fig.savefig(self.output_events+'/gamma_mf.pdf', bbox_inches='tight')
+        self.postprocess()
         return
 
 
