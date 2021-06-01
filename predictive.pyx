ce55ed2a926e170eb23fbca1a78f3e2bb6d7651c
Walter Del Pozzo <walter.delpozzo@ligo.org>
2021-06-01 14:41:43 +0200

    a few optimisation flags

--- predictive.pyx
+++ predictive.pyx
@@ -1,13 +1,12 @@
 import numpy as np
 cimport numpy as np
 from numpy cimport ndarray
-from libc.math cimport INFINITY
+from libc.math cimport INFINITY, sqrt, log, M_PI
 cimport cython
 from scipy.integrate import dblquad
 from scipy.special import logsumexp
 
-
-def log_numerical_predictive(events, m_min, m_max, sigma_min, sigma_max):
+def log_numerical_predictive(np.ndarray events, m_min, m_max, sigma_min, sigma_max):
     events_array = np.empty(len(events), dtype = object)
     for i in range(len(events)):
         ev = events[i]
@@ -17,13 +16,20 @@ def log_numerical_predictive(events, m_min, m_max, sigma_min, sigma_max):
         events_array[i] = components_array
     return _log_numerical_predictive(events_array, m_min, m_max, sigma_min, sigma_max)
 
-
-cdef _log_numerical_predictive(np.ndarray events, double m_min, double m_max, double sigma_min, double sigma_max):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef double _log_numerical_predictive(np.ndarray events, double m_min, double m_max, double sigma_min, double sigma_max):
     cdef double I, dI
     integrand = lambda sigma, mu : integrand_function(mu, sigma, events)
     I, dI = dblquad(integrand, m_min, m_max, gfun = lambda x: sigma_min, hfun = lambda x: sigma_max)
     return np.log(I)
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
 cdef double integrand_function(double mu, double sigma, np.ndarray events):
     cdef int i, j
     cdef double x = 0.
@@ -35,5 +41,9 @@ cdef double integrand_function(double mu, double sigma, np.ndarray events):
         x += y
     return np.exp(x)
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
 cdef inline double log_norm(double x, double x0, double sigma1, double sigma2):
-    return -((x-x0)**2)/(2*(sigma1**2 + sigma2**2)) - np.log(np.sqrt(2*np.pi)) - 0.5*np.log(sigma1**2 + sigma2**2)
+    return -((x-x0)**2)/(2*(sigma1**2 + sigma2**2)) - log(sqrt(2*M_PI)) - 0.5*log(sigma1**2 + sigma2**2)
