20165451e94faa99141fe2775a95014536f83059
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-06-15 11:32:31 +0200

    save

--- parest_test.py
+++ parest_test.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ParEst import DirichletDistribution
+from ParEst import DirichletDistribution, DirichletProcess
 import pickle
 import cpnest
 import corner
-from scipy.special import erf
+from scipy.special import erf, logsumexp
 from scipy.stats import norm, dirichlet
 from scipy.interpolate import interp1d
 import os
+
+
+def log_add(x, y): return x+np.log(1.0+np.exp(y-x)) if x >= y else y+np.log(1.0+np.exp(x-y))
+def log_norm(x, x0, s): return -((x-x0)**2)/(2*s*s) - np.log(np.sqrt(2*np.pi)) - np.log(s)
 
 def PL(m, alpha, m_max, m_min, l_max, l_min):
     f = m**(-alpha)*(1+erf((m-m_min)/(l_min)))*(1+erf((m_max-m)/l_max))/4.
@@ -16,53 +20,95 @@ def PL(m, alpha, m_max, m_min, l_max, l_min):
 def gauss(x, x0, s):
     return np.exp(-((x-x0)**2/(2*s**2)))/(np.sqrt(2*np.pi)*s)
 
-def logPrior(mu, sigma):
+def bimodal(x,x0,s0,x1,s1):
+    return (np.exp(-((x-x0)**2/(2*s0**2)))/(np.sqrt(2*np.pi)*s0) + np.exp(-((x-x1)**2/(2*s1**2)))/(np.sqrt(2*np.pi)*s1))/2.
+
+def logPrior(*args):
     return 0
  
-#true_vals = [1.2, 75, 20, 10, 5, 1]
-mu = 25
-s = 3
-a = 100
-true_vals = [mu, s, a]
+#
+alpha = 1.1
+mmax = 75
+mmin = 15
+lmax = 5
+lmin = 5
+mu = 30
+sigma = 6
+a = 20
+#true_vals = [alpha, mmax, mmin, lmax, lmin, a, 1]
+true_vals = [mu, sigma, a, 1]
+#true_vals = [50, 2, 100, 2.5, a, 1]
 
-#samp_file = '/Users/stefanorinaldi/Documents/mass_inference/PL_peak/reconstructed_events/pickle/posterior_functions_event_1.pkl'
-#openfile  = open(samp_file, 'rb')
-#samples   = pickle.load(openfile)
-#openfile.close()
+samp_file = '/Users/stefanorinaldi/Documents/mass_inference/DP/reconstructed_events/pickle/posterior_functions_event_0.pkl'
+openfile  = open(samp_file, 'rb')
+samples   = pickle.load(openfile)
+openfile.close()
 
-x = np.linspace(20,30,20)
+
+x = np.linspace(10, 50,300)
 dx = x[1]-x[0]
-ps = norm(loc = mu, scale = s).pdf(x)
 
-samples  = dirichlet(a*ps*dx/np.sum(ps*dx)).rvs(size = 30)
+##
+#ps = PL(x, alpha, mmax, mmin, lmax, lmin)
+#
+#samples  = dirichlet(a*ps*dx/np.sum(ps*dx)).rvs(size = 300)
+#
+out_folder  = '/Users/stefanorinaldi/Documents/parametric/DP'
+#
+#samples = np.array([s for s in samples if (s != 0.).all()])
+#
+#fig = plt.figure()
+#ax  = fig.add_subplot(111)
+#for s in samples:
+#    ax.plot(x, s, linewidth = 0.3)
+#
+#ax.plot(x,ps*dx/np.sum(ps*dx))
+#
+#ax.set_xlabel('x')
+#ax.set_ylabel('p(x)dx')
+#fig.suptitle('Conc. par. = {0}'.format(a))
+#plt.savefig(out_folder+'/draws.pdf', bbox_inches = 'tight')
 
-out_folder  = '/Users/stefanorinaldi/Documents/mass_inference/PL_no_peak/PE'
+#probs = []
+#for samp in samples:
+#    p = np.ones(300) * -np.inf
+#    for component in samp.values():
+#        logW = np.log(component['weight'])
+#        mu   = component['mean']
+#        s    = component['sigma']
+#        for i, mi in enumerate(x):
+#            p[i] = log_add(p[i], logW + log_norm(mi, mu, s))
+#    p = np.exp(p + np.log(dx) - logsumexp(p+np.log(dx)))
+#    probs.append(p)
+#
+#for p in probs:
+#    plt.plot(x,p, lw = 0.3)
+#
+#plt.plot(x, gauss(x, 30.075, 5.945)*dx/np.sum(gauss(x, 30.075, 5.945)*dx), c = 'r')
+#plt.show()
 
-fig = plt.figure()
-ax  = fig.add_subplot(111)
-for s in samples:
-    ax.plot(x, s, linewidth = 0.3)
-ax.plot(x,ps*dx/np.sum(ps*dx))
+#names  = ['alpha', 'm_max', 'm_min', 'l_max', 'l_min']
+#bounds = [[0,2], [60,80], [10,30], [3,10], [3,10]]
+#labels  = ['\\alpha', 'm_{max}', 'm_{min}', '\\lambda_{max}', '\\lambda_{min}']
+#
+#names = ['mu_1', 'sigma_1', 'mu_2', 'sigma_2']
+#bounds = [[40,60], [1,4], [90, 110], [1,4]]
+#labels = ['\mu_1', '\sigma_1', '\mu_2', '\sigma_2']
+names = ['mu', 'sigma']
+bounds = [[20,45], [3,7]]
+labels = ['\mu', '\sigma']
 
-ax.set_xlabel('x')
-ax.set_ylabel('p(x)dx')
-fig.suptitle('Conc. par. = {0}'.format(a))
-plt.savefig(out_folder+'/draws.pdf', bbox_inches = 'tight')
 
-names  = ['mu', 'sigma']#['alpha', 'm_max', 'm_min', 'l_max', 'l_min']
-bounds = [[23, 28], [2,4]]#[[0,2], [70,80], [15,30], [8,12], [3,7]]
-labels  = ['\\mu', '\\sigma']#['\\alpha', 'm_{max}', 'm_{min}', '\\lambda_{max}', '\\lambda_{min}']
-
-PE = DirichletDistribution(
+PE = DirichletProcess(
     gauss,
     names,
     bounds,
-    samples,
-    min(x),
-    max(x),
+    samples[:300],
+    10,
+    50,
     prior_pars = logPrior,
-    probs = samples,
-    n = len(samples[0])
+    max_a = 10000,
+    max_N = 200
     )
 
 work = cpnest.CPNest(PE,
@@ -75,8 +121,8 @@ work = cpnest.CPNest(PE,
 work.run()
 print('log Evidence: {0}'.format(work.NS.logZ))
 
-labels = labels + ['a']
-names = names + ['a']
+labels = labels + ['a', 'N']
+names = names + ['a','N']
 x = work.posterior_samples.ravel()
 samps = np.column_stack([x[lab] for lab in names])
 fig = corner.corner(samps,
--- universe_generator.py
+++ universe_generator.py
@@ -1,16 +1,17 @@
 import numpy as np
 from numpy.random import uniform, triangular, normal, gumbel
 from scipy.special import erf
+from scipy.interpolate import interp1d
 import os
 import matplotlib.pyplot as plt
 
 #def mass_function(m, alpha, m_max, m_min,scale_max=5, scale_min=5):
-##    return m**(-alpha)*(-alpha+1)/(m_max**(-alpha+1) - m_min**(-alpha+1))*(1-np.exp(-(m-m_max)/scale_max))*(1-np.exp(-(m_min-m)/scale_min))
-#def mass_function(m, alpha=1.1, m_max=70, m_min=15, l_max = 5, l_min = 5):
-#    return m**(-alpha)*(1+erf((m-m_min)/(l_min)))*(1+erf((m_max-m)/l_max))/4.
+#    return m**(-alpha)*(-alpha+1)/(m_max**(-alpha+1) - m_min**(-alpha+1))*(1-np.exp(-(m-m_max)/scale_max))*(1-np.exp(-(m_min-m)/scale_min))
+def mass_function(m, alpha=0, m_max=50, m_min= 20, l_max = 5, l_min = 5):
+    return m**(-alpha)*(1+erf((m-m_min)/(l_min)))*(1+erf((m_max-m)/l_max))/4.
 
-def mass_function(m, x01 = 50, sigma1 = 2, x02 = 100, sigma2 = 2.5):
-    return (np.exp(-(m-x01)**2/(2*sigma1**2))/(np.sqrt(2*np.pi)*sigma1) + np.exp(-(m-x02)**2/(2*sigma2**2))/(np.sqrt(2*np.pi)*sigma2))/2.
+#def mass_function(m, x01 = 40, sigma1 = 6, x02 = 60, sigma2 = 4.5):
+#    return (0.8*np.exp(-(m-x01)**2/(2*sigma1**2))/(np.sqrt(2*np.pi)*sigma1) + 0.2*np.exp(-(m-x02)**2/(2*sigma2**2))/(np.sqrt(2*np.pi)*sigma2))/2.
 
 
 def posterior_probability(m, m_true, k, b):
@@ -31,7 +32,7 @@ def posterior_sampler(m_true, sigma):
     
 if __name__ == '__main__':
 
-    out_folder = '/Users/stefanorinaldi/Documents/mass_inference/bimodal_200/'
+    out_folder = '/Users/stefanorinaldi/Documents/mass_inference/selfunc/'
     if not os.path.exists(out_folder):
         os.mkdir(out_folder)
     post_folder = out_folder+'/events/'
@@ -40,51 +41,53 @@ if __name__ == '__main__':
     if not os.path.exists(out_folder+'/events/'):
         os.mkdir(post_folder)
         
-        
-    n_bbh = 100
+    data_sf = np.genfromtxt(out_folder + 'selfunc.txt', names = True)
+    selfunc = interp1d(data_sf['m1'], data_sf['pdet']**3*(np.max( data_sf['pdet'])/np.max(data_sf['pdet']**3)))
+    
+    n_bbh = 250
     n_samples = 100
 
     alpha = 1.1
-    m_min = 30
-    m_max = 120
+    m_min = 16
+    m_max = 70
     s_min = 2
     s_max = 4
     
     app = np.linspace(m_min, m_max, 1000)
     mf  = mass_function(app)
     sup = mf.max()
-    norm = np.sum(mf*(app[1]-app[0]))
+    norm = np.sum(mf*selfunc(app)*(app[1]-app[0]))
 
     bbhs = []
     masses = []
     sigmas = []
     fig = plt.figure()
     ax  = fig.add_subplot(111)
-    for i in range(n_bbh):
-        m1 = normal(loc = 50, scale = 2)
-        sigma = np.exp(uniform(np.log(3), np.log(5)))
-        samples = normal(loc = m1, scale = sigma, size = n_samples)
-        np.savetxt(post_folder + '/event_{0}.txt'.format(i+1), np.array(samples))
-        bbhs.append(samples)
-        masses.append(m1)
-        sigmas.append(sigma)
-        
-    for i in range(n_bbh):
-        m1 = normal(loc = 100, scale = 2.5)
-        sigma = np.exp(uniform(np.log(3), np.log(5)))
-        samples = normal(loc = m1, scale = sigma, size = n_samples)
-        np.savetxt(post_folder + '/event_{0}.txt'.format(i+n_bbh+1), np.array(samples))
-        bbhs.append(samples)
-        masses.append(m1)
-        sigmas.append(sigma)
+    i = 0
+#    for i in range(n_bbh):
+    while i < n_bbh:
+        m1 = mass_sampler(m_max, m_min, sup)
+        if selfunc(m1) > uniform():
+            sigma = np.exp(uniform(np.log(3), np.log(5)))
+            samples = normal(loc = m1, scale = sigma, size = n_samples)
+            np.savetxt(post_folder + '/event_{0}.txt'.format(i+1), np.array(samples))
+            bbhs.append(samples)
+            masses.append(m1)
+            sigmas.append(sigma)
+            i += 1
+
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
 
     appM = np.linspace(m_min, m_max, 1000)
-    ax1.hist(masses, bins = 200, density = True)
-    ax1.plot(appM, mass_function(appM)/norm, color = 'r', linewidth = 0.5)
+    ax1.hist(masses, bins = int(np.sqrt(len(masses))), density = True)
+    ax1.plot(appM, mass_function(appM)*selfunc(appM)/norm, color = 'r', label = 'Observed')
+    ax1.plot(appM, mass_function(appM)/(np.sum(mass_function(appM))*(appM[1]-appM[0])), color = 'k', label = 'Astrophysical')
+#    ax1.plot(appM, selfunc(appM))
     ax1.set_xlabel('$M_1\ [M_\\odot]$')
+    ax1.legend(loc = 0)
+#    ax1.set_ylim(0, sup*(1.1))
     plt.tight_layout()
     plt.savefig(out_folder+'/truths.pdf', bbox_inches = 'tight')
     np.savetxt(out_folder+'/truths.txt', np.array([masses, sigmas]).T, header = 'm sigma')
